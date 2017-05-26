ae42147963a361207b1d166a4fb2b14dcd1d9700
tpctt <tim@timdeMac-mini.local>
2017-05-26 17:02:55 +0800

    framework

---
+++ NewUserGuide_Framework/NewUserGuide_Framework.h
@@ -0,0 +1,19 @@
+//
+//  NewUserGuide_Framework.h
+//  NewUserGuide_Framework
+//
+//  Created by tim on 17/5/26.
+//  Copyright © 2017年 timRabbit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NewUserGuide_Framework.
+FOUNDATION_EXPORT double NewUserGuide_FrameworkVersionNumber;
+
+//! Project version string for NewUserGuide_Framework.
+FOUNDATION_EXPORT const unsigned char NewUserGuide_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NewUserGuide_Framework/PublicHeader.h>
+
+
